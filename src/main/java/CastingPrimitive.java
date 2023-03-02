17e34091e1d3dc3a0cecbc6d9a1804412a617e2f
Ronny Barahona <ronnybarahona10@gmail.com>
2023-03-02 20:33:39 +0000

    hmm

--- src/main/java/CastingPrimitive.java
+++ src/main/java/CastingPrimitive.java
@@ -15,7 +15,8 @@ public class CastingPrimitive {
      * @param d a decimal number.
      * @return the int form of d.
      */
-    public int castToInt(double d){
-        return 0;
-    }
+    public int castToInt(double d) {
+    int result = (int) Math.floor(d);
+    return result;
+}
 }
